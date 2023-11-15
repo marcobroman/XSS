647d7fb2d4e971b578d2cae0b39fad0efe347de4
Marco Roman <roman.bmarco@gmail.com>
2023-11-15 13:15:31 -0600

    init

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     item = request.args.get("item","default")
-
+    if item == "default":
+        return "hola"
     if request.method == "GET":
         return item
+    
+    return
     
 if __name__ == "__main__":
     app.run(debug=True)
